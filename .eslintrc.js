7fdcb061da6df5af27b302fe9b7c897c0ad6d50e
Dan Vanderkam <danvdk@gmail.com>
2022-05-06 20:52:13 -0400

    Initial eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+
+      plugins: [
+        '@typescript-eslint',
+        // 'eslint-plugin-expect-type',
+      ],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        // 'expect-type/expect': 'error',
+      }
+    },
+  ],
+};
